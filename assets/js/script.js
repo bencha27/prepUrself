497b8714ca2d2203ce31dbb92ecf45446044b20b
BluSkreen <jasonduran303@gmail.com>
2022-11-09 13:17:36 -0700

    added to ajax function, and added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@
 // Variables for form selects and IDs
 var searchBoxEl = $("#search-box");
 // bellow is inside <section id="search-box">
-var searchForm = $("#search-form");
+var searchFormEl = $("#search-form");
 // bellow is inside <form id="search-form"></form>
 var searchInputEl = $("#search-input");
 var dietInputEl = $("#diet-input");
@@ -25,13 +25,15 @@ var fridayEl = $("#search-box");
 var saturdayEl = $("#search-box");
 var sundayEl = $("#search-box");
 
+//examples
 //https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=0b97683e&app_key=8b9f9e545d235c4ae37ac9cbb16a65a5&diet=low-carb
 
 //https://api.edamam.com/api/recipes/v2?type=public&app_id=0b97683e&app_key=8b9f9e545d235c4ae37ac9cbb16a65a5&q=chicken&diet=low-carb
 
 // The endpoint with only id and key parameters
 var formSearchEdamamURL = "https://api.edamam.com/api/recipes/v2?type=public&app_id=0b97683e&app_key=8b9f9e545d235c4ae37ac9cbb16a65a5"
-// 
+
+//  fetch vs ajax
 // fetch()
 //   .then(function (response) {
 //     return response.json();
@@ -40,3 +42,46 @@ var formSearchEdamamURL = "https://api.edamam.com/api/recipes/v2?type=public&app
 //     console.log(data);
 //   });
 
+// $.ajax({
+//   url: "/api/getWeather",
+//   data: {
+//     parameter: thing
+//   },
+//   success: function( result ) {
+//     $( "#element" ).html( "<strong>" + result + "</strong> degrees" );
+//   }
+// });
+
+main
+$(function() {
+    searchButtonEl.on("click", ajaxQueryData());
+
+});
+
+
+var ajaxQueryData = function(event) {
+    event.preventDefault();
+    //collect and store parameters
+    var parameterObj = {};
+    $(searchFormEl).children().each(function () {
+        var inputID = $(this).attr("id");
+        
+
+    });
+    // 
+}
+
+
+// ajax parameter object example
+// var parameterObj = {
+//     q: "chicken",
+//     diet: "low-carb"
+// };
+
+// $.ajax({
+//   url: "https://api.edamam.com/api/recipes/v2?type=public&app_id=0b97683e&app_key=8b9f9e545d235c4ae37ac9cbb16a65a5",
+//   data: parameterObj,
+//   success: function( result ) {
+//     console.log(result);
+//   }
+// });
