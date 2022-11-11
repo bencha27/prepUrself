8815569a01d1c2cb8a3dca7b3de0cc93ca158005
BluSkreen <jasonduran303@gmail.com>
2022-11-10 19:53:57 -0700

    minor changes to comments, added selections id

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,24 +12,18 @@ function getPhotoApi() {
     })    
 }
 
-getPhotoApi();
+// getPhotoApi();
 
-
-
-
-// Variables for form selects and IDs
-var searchBoxEl = $("#search-box");
-// bellow is inside <form id="search-form"></form>
-var searchInputEl = $("#search-input");
-var dietInputEl = $("#diet-input");
-var healthInputEl = $("#health-input");
-var cuisineTypeInputEl = $("#cuisineType-input");
-var mealTypeInputEl = $("#mealType-input");
-var dishTypeInputEl = $("#dishType-input");
+//form
+var searchFormEl = $("#search-form");
+var selectionsEl = $("#selections");
+var searchInputEl = $("#q");
 var searchButtonEl = $("#search-button");
-// <section id="search-results"></section>
-var searchResultsEl = $("#search-results");
-// bellow is inside <aside>
+var searchBarEl = $("#search-bar");
+//results
+var resultsContainerEl = $("resultsContainer");
+var searchResultsEl = $("");
+// aside
 var scheduleEl = $("#schedule");
 var mondayEl = $("#monday");
 var tuesdayEl = $("#tuesday");
@@ -45,15 +39,17 @@ var sundayEl = $("#sunday");
 //https://api.edamam.com/api/recipes/v2?type=public&app_id=0b97683e&app_key=8b9f9e545d235c4ae37ac9cbb16a65a5&q=chicken&diet=low-carb
 
 // The endpoint with only id and key
-var formSearchEdamamURL =
+var edamamEndpoint =
   "https://api.edamam.com/api/recipes/v2?type=public&app_id=0b97683e&app_key=8b9f9e545d235c4ae37ac9cbb16a65a5";
 
-// main
+// main (listen for page inputs)
+// runs when DOM is ready
 $(function () {
   // load the home page on landing
   // loadHomePage();
 
   // listen for user to search
+  searchFormEl.on("submit", loadResultsPage);
   searchButtonEl.on("click", loadResultsPage);
 
   // listen for user to go home
@@ -61,52 +57,69 @@ $(function () {
 
   // listen for user to got to calendar
   // loadCalendarPage();
-});
+}); // main end
 
+function loadHomePage() {
+  $(searchBarEl).css({ "margin-top": "350px" });
+  // $(resultsContainerEl).hide();
+  // $(resultsContainerEl).css("display", "none");
+}
+
+// -------- RESULTS PAGE ---------------
 // call ajaxQueryData
 // position page elements
 // fill in results with data
 function loadResultsPage(event) {
   // postion elements
-  // positionResultsPage();
+  positionResultsPage();
 
   // query data
   var apiObject = ajaxQueryData(event);
   console.log(apiObject);
 
   // fill results
-  fillSearchResults(apiObject);
+  // fillSearchResults(apiObject);
+}
+
+function positionResultsPage() {
+  $(searchBarEl).css({ "margin-top": "20px" });
+  $(resultsContainerEl).show();
 }
 
 function ajaxQueryData(event) {
   event.preventDefault();
   //collect and store parameters
   var parameterObj = {};
-  $(searchBoxEl)
+  // Go through filter selections
+  $(selectionsEl)
     .children()
     .each(function () {
-      // console.log("This.val() in .each: " + $(this).val());
       // If the option($(this)) has a value than add it to the paramater object
-      if (
-        $(this).val() != "" &&
-        $(this).attr("id") != "search-button" &&
-        $(this).val() != null &&
-        $(this).val() != "none"
-      ) {
+      if ($(this).val() != "" && $(this).val() != null && $(this).val() != "none") {
         var inputID = $(this).attr("id");
         var inputValue = $(this).val();
         // use options id and value as key pairs for parameters
         parameterObj[inputID] = inputValue;
-        // console.log("val()This. in if(): " + $(this).val());
       }
     });
 
+  // if user inputs string, use "q" for query param
+  if (
+    $(searchInputEl).val() != "" &&
+    $(searchInputEl).val() != null &&
+    $(searchInputEl).val() != "none"
+  ) {
+    var inputID = $(searchInputEl).attr("id");
+    var inputValue = $(searchInputEl).val();
+    parameterObj[inputID] = inputValue;
+  }
   // console.log(parameterObj); // Shows parameters
+
   // use paramater obj to query
   var storeResults;
   $.ajax({
     async: false,
-    url: "https://api.edamam.com/api/recipes/v2?type=public&app_id=0b97683e&app_key=8b9f9e545d235c4ae37ac9cbb16a65a5",
+    url: edamamEndpoint,
     data: parameterObj,
     success: function (result) {
       // console.log(result); // SHOULD SHOW API OBJECT WITH RECIPES
@@ -114,16 +127,20 @@ function ajaxQueryData(event) {
       storeResults = result.hits;
     },
   });
-
   return storeResults;
 }
 
-function fillSearchResults(results) {
-  // For each card in #search-results
-  var cardNumber = 0;
-  $("#search-results")
-    .children()
-    .each(function () {
-      cardNumber++;
-    });
-}
+// var fillSearchResults = function (results) {
+//   // For each card in #search-results
+//   var cardNumber = 0;
+//   $(searchResultsEl)
+//     .children()
+//     .each(function () {
+//       cardNumber++;
+//     });
+// };
+// RESULTS PAGE END
+
+// -----------------------------------
+
+// --------CALENDAR PAGE-------------
